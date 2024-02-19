1e49f1e4bdd79d09ce3eeb82f9fd0f24725cf9af
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-02-19 12:47:12 +0000

    added/pokemon-variables
    5 different pokemon types were added to the pokemon list

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,5 @@
-alert('Hello world');
-let favoriteFood= ("Pizza");
-document.write(favoriteFood);
+let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 lbs'},
+                   {name: 'Charmander', type: 'fire', height: 2.00, weight:'18.7 lbs'},
+                   {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
+                   {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
+                   {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
