8ed83fbbd536b7a6d466d68536017ea5e9128c54
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-03-19 12:41:04 +0100

    update to a for each loop
    Update from a for loop to a for each loop

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 
                    {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
                    {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
                    {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
-for (let i=0; i<pokemonList.length; i++) {
-    document.write (pokemonList[i].name + ' (height: '+ pokemonList[i].height +') ');
-if (pokemonList[i].height<= 1.0) {
+pokemonList.forEach (function (pokemon) {
+    document.write (pokemon.name + " is " + pokemon.height + " meters tall");
+if (pokemon.height<= 1.0) {
     document.write ('<br>');
 }
 else {
-    document.write ('That\'s a big pokemon!');
+    document.write (' - Wow, that\'s a big pokemon!');
     document.write ('<br>');
 }
-}
+})
