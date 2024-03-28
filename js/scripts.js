d44f3f5d05ddb1a659960f1558be74dc0c6d40c3
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-03-28 10:10:44 +0100

    updated DOM code
    error fixes

--- js/scripts.js
+++ js/scripts.js
@@ -3,13 +3,13 @@ let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 
                    {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
                    {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
                    {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
-let pokemonRepository = (function () {
+ pokemonRepository.getAll().forEach(function(pokemon){
 const pokemonList = [
                     {
                         name: 'Squirtle', 
                         type: 'water', 
                         height: 1.08, 
-                        weight:'19.8 lbs'
+                        weight: '19.8 lbs'
                     },
                     {
                         name: 'Charmander', 
@@ -21,32 +21,32 @@ const pokemonList = [
                         name: 'Rattata', 
                         type: 'normal', 
                         height: 1.00, 
-                        weight:'7.7 lbs'
+                        weight: '7.7 lbs'
                     },
                     {
                         name: 'Pikachu', 
                         type: 'electric', 
                         height: 1.04, 
-                        weight:'13.2 lbs'
+                        weight: '13.2 lbs'
                     },                        
                     {
                         name: 'Weedle', 
                         type: ['bug', 'poison'], 
                         height: 1.00, 
-                        weight:'7.1 lbs'
+                        weight: '7.1 lbs'
                     } 
             ]
-    function add (pokemon) {
+    function add(pokemon) {
         pokemonList.push (pokemon);
     }             
-    function getAll () {
+    function getAll() {
         return pokemonList;
     }
-    return {
-        add,
-        getAll,
-    };
-})();
+   // return {
+     //   add,
+       // getAll,
+   // };
+//})();
 //const pokemonLoop = 
 function addListItem(pokemon) {
     let pokemonList= document.querySelector('.pokemon-list');
@@ -58,7 +58,10 @@ function addListItem(pokemon) {
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);   
 };
-        pokemonRepository.getAll().forEach(function(pokemon){
-          //pokemonRepository.addListItem(pokemon);
-        });
- 
+return {
+    add,
+    getAll,
+    addListItem
+};
+        //pokemonRepository.getAll().forEach(function(pokemon){
+ })();
