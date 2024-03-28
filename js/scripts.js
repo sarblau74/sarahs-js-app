dbc85e0aa7c06cd31af488a46bef88cf8329c1ae
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-03-28 22:32:27 +0100

    bug fixes
    small details added and deleted

--- js/scripts.js
+++ js/scripts.js
@@ -1,78 +1,74 @@
-//let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 lbs'},
- //                  {name: 'Charmander', type: 'fire', height: 2.00, weight:'18.7 lbs'},
-   //                {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
-     //              {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
-       //            {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
 let pokemonRepository = (function () {
-const pokemonList = [
-                    {
-                        name: 'Squirtle', 
-                        type: 'water', 
-                        height: 1.08, 
-                        weight: '19.8 lbs'
-                    },
-                    {
-                        name: 'Charmander', 
-                        type: 'fire', 
-                        height: 2.00, 
-                        weight:'18.7 lbs'
-                    },
-                    {
-                        name: 'Rattata', 
-                        type: 'normal', 
-                        height: 1.00, 
-                        weight: '7.7 lbs'
-                    },
-                    {
-                        name: 'Pikachu', 
-                        type: 'electric', 
-                        height: 1.04, 
-                        weight: '13.2 lbs'
-                    },                        
-                    {
-                        name: 'Weedle', 
-                        type: ['bug', 'poison'], 
-                        height: 1.00, 
-                        weight: '7.1 lbs'
-                    } 
-            ];
+    const pokemonList = [
+        {
+            name: 'Squirtle', 
+            type: 'water', 
+            height: 1.08, 
+            weight: '19.8 lbs'
+        },
+        {
+            name: 'Charmander', 
+            type: 'fire', 
+            height: 2.00, 
+            weight:'18.7 lbs'
+        },
+        {
+            name: 'Rattata', 
+            type: 'normal', 
+            height: 1.00, 
+            weight: '7.7 lbs'
+        },
+        {
+            name: 'Pikachu', 
+            type: 'electric', 
+            height: 1.04, 
+            weight: '13.2 lbs'
+        },                        
+        {
+            name: 'Weedle', 
+            type: ['bug', 'poison'], 
+            height: 1.00, 
+            weight: '7.1 lbs'
+        } 
+    ];
+
     function add(pokemon) {
-        pokemonList.push (pokemon);
+        pokemonList.push(pokemon);
     }           
+    
     function getAll() {
         return pokemonList;
     }
 
-function addListItem(pokemon) {
-    let pokemonList= document.querySelector('.pokemon-list');
-        let listItem= document.createElement('li');
-        let button= document.createElement ('button');
+    function addListItem(pokemon) {
+        let pokemonList = document.querySelector('.pokemon-list');
+        let listItem = document.createElement('li');
+        let button = document.createElement('button');
         
-        button.innerText= pokemon.name;
+        button.innerText = pokemon.name;
         console.log(button.innerText);
-        button.classList.add ('pokemon-button');
+        button.classList.add('pokemon-button');
 
         listItem.appendChild(button);
         pokemonList.appendChild(listItem); 
-        
-        
+
         addEventListenerToButton(button, pokemon);
     }
 
     function addEventListenerToButton(button, pokemon) {
         button.addEventListener('click', function () {
             showDetails(pokemon);
-        })
+        });
+
         function showDetails(pokemon) {
             console.log(pokemon);
-}
+        }
     } 
-return {
-    add,
-    getAll,
-    addListItem
-};
 
- pokemonRepository.getAll().forEach(function(pokemon){
- })()
-})
+    return {
+        add,
+        getAll,
+        addListItem
+    };
+})(); 
+
