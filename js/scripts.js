bb1c3c3ff20510173008e51bcc8637530054eb96
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-03-28 14:03:40 +0100

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,9 @@
-let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 lbs'},
-                   {name: 'Charmander', type: 'fire', height: 2.00, weight:'18.7 lbs'},
-                   {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
-                   {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
-                   {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
- pokemonRepository.getAll().forEach(function(pokemon){
+//let pokemonList = [{name: 'Squirtle', type: 'water', height: 1.08, weight:'19.8 lbs'},
+ //                  {name: 'Charmander', type: 'fire', height: 2.00, weight:'18.7 lbs'},
+   //                {name: 'Rattata', type: 'normal', height: 1.00, weight:'7.7 lbs'},
+     //              {name: 'Pikachu', type: 'electric', height: 1.04, weight:'13.2 lbs'},
+       //            {name: 'Weedle', type: ['bug', 'poison'], height: 1.00, weight:'7.1 lbs'}]
+let pokemonRepository = (function () {
 const pokemonList = [
                     {
                         name: 'Squirtle', 
@@ -35,19 +35,14 @@ const pokemonList = [
                         height: 1.00, 
                         weight: '7.1 lbs'
                     } 
-            ]
+            ];
     function add(pokemon) {
         pokemonList.push (pokemon);
-    }             
+    }           
     function getAll() {
         return pokemonList;
     }
-   // return {
-     //   add,
-       // getAll,
-   // };
-//})();
-//const pokemonLoop = 
+
 function addListItem(pokemon) {
     let pokemonList= document.querySelector('.pokemon-list');
         let listItem= document.createElement('li');
@@ -57,11 +52,11 @@ function addListItem(pokemon) {
         button.classList.add ('pokemon-button');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);   
-};
+}
 return {
     add,
     getAll,
     addListItem
 };
         //pokemonRepository.getAll().forEach(function(pokemon){
- })();
+ })()
