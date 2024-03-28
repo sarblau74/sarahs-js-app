8dae9107a55ef7011d97abaa1903c7f1fdee05f8
sarblau74 <158480287+sarblau74@users.noreply.github.com>
2024-03-28 21:34:16 +0100

    Added EventListener
    event listener was added to show details of pokemon.

--- js/scripts.js
+++ js/scripts.js
@@ -47,16 +47,32 @@ function addListItem(pokemon) {
     let pokemonList= document.querySelector('.pokemon-list');
         let listItem= document.createElement('li');
         let button= document.createElement ('button');
+        
         button.innerText= pokemon.name;
         console.log(button.innerText);
         button.classList.add ('pokemon-button');
+
         listItem.appendChild(button);
-        pokemonList.appendChild(listItem);   
+        pokemonList.appendChild(listItem); 
+        
+        
+        addEventListenerToButton(button, pokemon);
+    }
+
+    function addEventListenerToButton(button, pokemon) {
+        button.addEventListener('click', function () {
+            showDetails(pokemon);
+        })
+        function showDetails(pokemon) {
+            console.log(pokemon);
 }
+    } 
 return {
     add,
     getAll,
     addListItem
 };
-        //pokemonRepository.getAll().forEach(function(pokemon){
+
+ pokemonRepository.getAll().forEach(function(pokemon){
  })()
+})
